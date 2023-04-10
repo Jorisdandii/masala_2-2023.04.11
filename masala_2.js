bb23bac075c6e851699f931f69a987246ac7ed62
Sirojiddin <jorisdandii@gmail.com>
2023-04-11 01:18:52 +0900

    first commit

---
+++ masala_2.js
@@ -0,0 +1,46 @@
+// -----------------------------------------------------------------------------------------------------------------
+//  2023.04.11 buguncha xozir shu masalalarni ko'rdim. In shaa Alloh xar kuni shu tartibdan va bundan ham kuchliroq 
+//  masalalar yechib qo'yib borishga xarakat qilaman. 
+// ------------------------------------------------------------------------------------------------------------------
+
+// Bu fuinctionda biz parametrimiz true bo'lsa Yes deb qaytaradi agar false bo'lsa No deb qaytaradi.
+// Diclaration function da bajarildi.
+
+// function boolToWord(bool) {
+//   let str = bool === true ? "Yes" : "No";
+//   return str
+// }
+// ******************************************************************************************************
+
+//Arrow function da bajarildi. 
+
+// const boolToWord = bool => bool === true ? "Yes" : "No";
+
+// ******************************************************************************************************
+
+//  NUmberlarimizni string ga aylantirib beradigan metod.
+
+// function numberToString(num) {
+//   return num.toString();
+// }
+
+// *******************************************************************************************************
+//  Bu function da juft son bo'lsa `Even` qaytaradi bo'lmasa `Odd` qaytaradi.
+
+// function evenOrOdd(number) {
+//  return number % 2 === 0 ? `Even` : `Odd`;
+// }
+//  console.log(evenOrOdd(3));
+
+// *******************************************************************************************************
+
+// Bu function da ham shartimiz qoniqtirsa `Yes` qaytaradi bo'lmasa `No` qaytaradi.
+
+// let bool_1 = 0;
+// const boolToWord = (bool) => {
+//     return bool_1 === true || bool_1 !== 0 ? 'Yes' : 'No'
+// }
+
+// console.log(boolToWord());
+
+// ********************************************************************************************************
