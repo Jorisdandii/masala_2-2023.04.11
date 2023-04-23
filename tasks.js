2befa29ce81dc5d3be44fdf280f0ba41c24f21c2
SIROJIDDIN <123286500+Jorisdandii@users.noreply.github.com>
2023-04-23 15:06:12 +0900

    codewars 2023.0423

---
+++ tasks.js
@@ -0,0 +1,23 @@
+// ****************2023.04.23**************************************
+
+// let arr = [1, 2, 3, 4, 5]
+// var summation = function () {
+//   let str = new Array(arr).fill(0).reduce((a, _, b) => a + b + 1, 0);
+//   console.log(str);
+// };
+// summation()
+
+// var summation = function (num) {
+//   let sum = 0;
+
+//   for (let i = 1; i <= num; i++) {
+//     sum += num;
+//     console.log(sum);
+//   }
+// };
+// summation(5);
+
+// const array = (arr) => {
+//    return arr.filter((value, index, array) => array.indexOf(value) === index);
+// }
+// console.log(array([1, 1, 2, 3, 3, 4, 3, 2, 5, 6, 6, 6, 6]));
